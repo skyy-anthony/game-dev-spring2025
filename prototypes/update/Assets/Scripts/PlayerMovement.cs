f4709d648ce21cb716fad137e8bfdfa7b17d40af
skyyanthony <skylarj.anthony@gmail.com>
2025-05-07 13:49:39 -0400

    PEOPLE

--- prototypes/update/Assets/Scripts/PlayerMovement.cs
+++ prototypes/update/Assets/Scripts/PlayerMovement.cs
@@ -1,57 +1,24 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public float jumpForce = 5f;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    private Rigidbody rb;
-    private bool isGrounded;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    }
 
     void Update()
     {
-        // Ground check
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
 
-        Vector3 movement = movementDirection * speed;
-        Vector3 newVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z); // Fixed to use rb.velocity
-        rb.linearVelocity = newVelocity;
-
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
         }
     }
 }
